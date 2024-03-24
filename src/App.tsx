f8bda694330a60357fa52c231fac4f3c116be410
Jenny Pieloor <jennypieloor.connect@gmail.com>
2024-03-24 23:15:10 +0800

    messaging-app

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/*Main Container*/}
+      <div>
+
+        {/*Message Group*/}
+        <div>
+
+        </div>
+
+
+        {/*Chat Screen*/}
+        <div>
+
+        </div>
+
+
+        {/*Info*/}
+        <div>
+
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
