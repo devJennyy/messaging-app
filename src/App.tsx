10b392bc1deedb3200ea41549d716673b397b0d3
Jenny Pieloor <jennypieloor.connect@gmail.com>
2024-03-26 06:35:34 +0800

    Finished Chat Info UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { BiSolidEditAlt } from "react-icons/bi";
 import { TbDotsVertical } from "react-icons/tb";
 import { BsEmojiWink } from "react-icons/bs";
 import { IoIosSend } from "react-icons/io";
-import { MdKeyboardVoice } from "react-icons/md";
+import { MdArrowForwardIos, MdDelete, MdKeyboardVoice } from "react-icons/md";
 import { TiAttachment } from "react-icons/ti";
+import { FiDownload } from "react-icons/fi";
 
 function App() {
   return (
@@ -61,7 +62,7 @@ function App() {
         </div>
 
         {/*Chat Screen - Container*/}
-        <div className="flex flex-col justify-center items-center w-[819px] p-5 bg-white">
+        <div className="flex flex-col justify-center items-center w-[820px] p-5 bg-white">
           <div className="flex flex-row justify-start items-center w-full gap-4  border-b-[1px] border-[#DEDCDC] pb-[15px]">
             <div className="w-[48px] h-[48px]">
               <img
@@ -72,11 +73,13 @@ function App() {
             <div className="flex flex-col justify-start items-start">
               <p className="text-[20px] font-medium text-[#323232]">Power</p>
               <p className="text-[14px] font-normal text-[#707070]">
-                Chainsawman
+                Chainsaw Man
               </p>
             </div>
-            <div className="flex flex-1 flex-row justify-end items-end text-[24px] text-[#323232]">
-              <TbDotsVertical />
+            <div className="flex flex-1 flex-row justify-end items-end">
+              <button className="text-[24px] text-[#323232] hover:scale-95 transition-all">
+                <TbDotsVertical />
+              </button>
             </div>
           </div>
 
@@ -216,17 +219,17 @@ function App() {
           {/*Chat Screen - Input Field*/}
           <div className="flex items-center relative w-[750px] pt-10 pb-3">
             <div className="flex justify-center items-center gap-2 absolute right-0 text-[20px] mr-4">
-              <button className="text-[17px]">
+              <button className="text-[17px] hover:scale-95 transition-all">
                 <BsEmojiWink />
               </button>
-              <button>
+              <button className="hover:scale-95 transition-all">
                 <MdKeyboardVoice />
               </button>
-              <button>
+              <button className="hover:scale-95 transition-all">
                 <TiAttachment />
               </button>
               <div className="flex justify-center items-center border-l-2 pl-2">
-                <button>
+                <button className="hover:scale-95 transition-all">
                   <IoIosSend />
                 </button>
               </div>
@@ -238,11 +241,157 @@ function App() {
           </div>
         </div>
 
-        {/*Info*/}
-        <div className="w-[500px] bg-white">
-          <p>Jenny Pieloor</p>
-          <p>Jenny Pieloor</p>
-          <p>Jenny Pieloor</p>
+        {/*Chat Info*/}
+        <div className="flex flex-col items-center w-[450px] h-full bg-white pt-10 px-10 ">
+          <div className="w-full">
+            <div className="flex flex-col justify-center items-center">
+              <div className="w-[134px] h-[134px] border-[6px] border-[#9C2B23] rounded-full">
+                <img
+                  src="/images/power-1.jpeg"
+                  className="w-full h-full object-cover rounded-full border-[5px] border-white"
+                />
+                <div className="text-[#535353] pt-5">
+                  <p className="text-[20px] font-medium">Power</p>
+                  <p className="text-[14px] font-normal">Chainsaw Man</p>
+                </div>
+              </div>
+            </div>
+
+            {/*Chat Info - Attachments*/}
+            <div className="flex flex-col justify-center">
+              <div className="flex flex-row justify-between items-center pt-28 text-[#323232]">
+                <p className="text-[14px] font-normal">Attachments</p>
+                <p className="text-[11px] font-normal underline">See all</p>
+              </div>
+
+              <div className="flex flex-row justify-start items-center pt-2">
+                <div className="flex flex-row justify-start items-center bg-[#F8F7F7] w-[430px] h-[70px] gap-4 pl-5">
+                  <img src="/images/pdf-file-icon.png" />
+                  <div className="flex flex-col justify-start items-start gap-2">
+                    <p className="text-[12px] font-medium text-[#324054]">
+                      File Title.pdf
+                    </p>
+                    <p className="text-[10px] font-regular text-[#71839B]">
+                      313 KB . 22 Dec, 2022
+                    </p>
+                  </div>
+                  <div className="flex flex-row justify-end absolute right-0 mr-16 items-end gap-3">
+                    <FiDownload className="text-[#71839B] text-[15px]" />
+                    <MdDelete className="text-[#A64039] text-[15px]" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-row justify-start items-center pt-2">
+                <div className="flex flex-row justify-start items-center bg-[#F8F7F7] w-[430px] h-[70px] gap-4 pl-5">
+                  <img src="/images/pdf-file-icon.png" />
+                  <div className="flex flex-col justify-start items-start gap-2">
+                    <p className="text-[12px] font-medium text-[#324054]">
+                      File Title.pdf
+                    </p>
+                    <p className="text-[10px] font-regular text-[#71839B]">
+                      313 KB . 22 Dec, 2022
+                    </p>
+                  </div>
+                  <div className="flex flex-row justify-end absolute right-0 mr-16 items-end gap-3">
+                    <FiDownload className="text-[#71839B] text-[15px]" />
+                    <MdDelete className="text-[#A64039] text-[15px]" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-row justify-start items-center pt-2">
+                <div className="flex flex-row justify-start items-center bg-[#F8F7F7] w-[430px] h-[70px] gap-4 pl-5">
+                  <img src="/images/pdf-file-icon.png" />
+                  <div className="flex flex-col justify-start items-start gap-2">
+                    <p className="text-[12px] font-medium text-[#324054]">
+                      File Title.pdf
+                    </p>
+                    <p className="text-[10px] font-regular text-[#71839B]">
+                      313 KB . 22 Dec, 2022
+                    </p>
+                  </div>
+                  <div className="flex flex-row justify-end absolute right-0 mr-16 items-end gap-3">
+                    <FiDownload className="text-[#71839B] text-[15px]" />
+                    <MdDelete className="text-[#A64039] text-[15px]" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/*Chat Info - Media*/}
+            <div>
+              <div className="flex flex-row justify-between items-center pt-5">
+                <p className="text-[14px] text-[#323232]">Media</p>
+                <MdArrowForwardIos className="text-[12px] text-[#323232]" />
+              </div>
+
+              <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-row justify-center items-center gap-1">
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-1.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-2.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-3.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-4.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-row justify-center items-center gap-1 mt-[-4px]">
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-1.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-2.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-3.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                  <div className="w-[90px] pt-2">
+                    <img
+                      src="/images/power-4.jpeg"
+                      className="w-full h-[90px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-row justify-center items-center gap-1 mt-[-4px]">
+                  <div className="w-[374px] pt-2">
+                    <img
+                      src="/images/power-1.jpeg"
+                      className="w-full h-[130px] object-cover rounded-[7px]"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
