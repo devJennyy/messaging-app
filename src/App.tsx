53ccbdfa839f7c1f8c1f9111fc87f1b2d77f3dbb
Jenny Pieloor <jennypieloor.connect@gmail.com>
2024-04-18 23:48:43 +0800

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import { TbDotsVertical, TbPlus } from "react-icons/tb";
-import { BsEmojiWink } from "react-icons/bs";
-import { IoIosSend, IoLogoGithub } from "react-icons/io";
+import { BsEmojiWink, BsMoonStars } from "react-icons/bs";
+import {
+  IoIosSend,
+  IoLogoGithub,
+  IoMdRefresh,
+} from "react-icons/io";
 import { MdArrowForwardIos, MdDelete, MdKeyboardVoice } from "react-icons/md";
 import { TiAttachment } from "react-icons/ti";
 import { FiDownload } from "react-icons/fi";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoSunny } from "react-icons/io5";
+import { FaRegStar } from "react-icons/fa";
+import {
+  HiOutlineArrowSmLeft,
+  HiOutlineArrowSmRight,
+} from "react-icons/hi";
+import { LuSearch } from "react-icons/lu";
 
 function App() {
   return (
@@ -13,7 +23,7 @@ function App() {
       {/*Background */}
       <div className="flex justify-center items-center min-w-[1240px] h-full bg-[#FAF7F2]">
         {/* Background */}
-        <div className="flex flex-col w-full h-full overflow-hidden relative hidden">
+        <div className="flex flex-col w-full h-full overflow-hidden relative">
           <div className="flex flex-row justify-between gap-10 ml-10 absolute">
             <img
               src="/images/vector-one.svg"
@@ -42,44 +52,81 @@ function App() {
         </div>
 
         {/* Main Container */}
-        <div className="flex justify-center w-[1240px] h-[700px] border border-[#BABCBE] bg-white text-[#3D4043]  absolute rounded-md">
-          <div className="flex flex-row justify-start items-end px-6 w-full h-10 bg-[#DFE1E6] rounded-t-md">
-
-            <div className="flex justify-center items-center bg-white w-16">
-              <div className="bg-[#DFE1E6] w-full h-full rounded-br-lg">
-                <img src="/images/dot-logo.svg" className="w-10 h-10"/>
+        <div className="flex flex-col w-[1240px] h-[700px] border border-[#BABCBE] bg-white text-[#3D4043] absolute rounded-md">
+          <div>
+            <div className="flex flex-row justify-start items-end px-6 w-full h-10 bg-[#DFE1E6] rounded-t-md">
+              <div className="flex justify-center items-center bg-white w-16">
+                <div className="bg-[#DFE1E6] w-full h-full rounded-br-lg">
+                  <img src="/images/dot-logo.svg" className="w-10 h-10" />
+                </div>
               </div>
-            </div>
 
-            <div className="flex flex-row justify-between items-center bg-white rounded-t-lg w-48 h-8">
-              <div className="flex flex-row justify-between items-center bg-white w-full h-full rounded-t-lg px-2">
-                <div className="flex flex-row justify-center items-center gap-2">
-                  <img src="/logo.svg" className="w-4 h-4"/>
-                  <p className="text-[12px]">Messaging App</p>
+              <div className="flex flex-row justify-between items-center bg-white rounded-t-lg w-48 h-8">
+                <div className="flex flex-row justify-between items-center bg-white w-full h-full rounded-t-lg px-2">
+                  <div className="flex flex-row justify-center items-center gap-2">
+                    <img src="/logo.svg" className="w-3 h-3" />
+                    <p className="text-[12px]">Messaging App</p>
+                  </div>
+
+                  <IoClose />
+                </div>
+              </div>
+
+              <div className="flex flex-row justify-between items-center bg-white w-48 h-8">
+                <div className="flex flex-row justify-between items-center bg-[#DFE1E6] w-full h-full rounded-bl-lg px-2">
+                  <div className="flex flex-row justify-center items-center gap-2">
+                    <IoLogoGithub />
+                    <p className="text-[12px]">devJennny</p>
+                  </div>
+
+                  <IoClose />
                 </div>
 
-                <IoClose />
-              </div>
-            </div>
-
-            <div className="flex flex-row justify-between items-center bg-white w-48 h-8">
-              <div className="flex flex-row justify-between items-center bg-[#DFE1E6] w-full h-full rounded-bl-lg px-2">
-                <div className="flex flex-row justify-center items-center gap-2">
-                  <IoLogoGithub />
-                  <p className="text-[12px]">devJennny</p>
+                <div className=" flex justify-center items-center h-full bg-[#DFE1E6]">
+                  <div className="border-r border-[#8B8E92] h-5"></div>
                 </div>
-
-                <IoClose />
               </div>
 
-              <div className=" flex justify-center items-center h-full bg-[#DFE1E6]">
-                <div className="border-r border-[#8B8E92] h-5"></div>
+              <div className="flex justify-center items-center w-10 h-8">
+                <TbPlus size={17} />
               </div>
             </div>
 
-            <div className="flex justify-center items-center w-10 h-8">
-            <TbPlus size={17} />
+            <div className="flex flex-row justify-between items-center w-full h-10 px-5 gap-2">
+              <div className="flex flex-row justify-center items-center gap-2 mr-2">
+                <HiOutlineArrowSmLeft size={20} />
+                <HiOutlineArrowSmRight size={20} />
+                <IoMdRefresh size={17} />
+              </div>
+
+              <div className="flex flex-row justify-between items-center w-full h-7 relative">
+                <input
+                  placeholder="Search or enter web address"
+                  className="w-full h-full bg-[#F1F3F4] rounded-full pl-9 pr-9 text-[12px] relative outline-none"
+                />
+                <LuSearch className="absolute ml-3" />
+                <FaRegStar size={15} className="absolute right-0 mr-3" />
+              </div>
+
+              <div className="flex justify-between items-center w-16 h-7 bg-[#F1F3F4] rounded-full px-1 shadow-sm">
+                <div className="flex justify-between items-center w-6 h-6 rounded-full bg-white">
+                  <IoSunny className="w-full" />
+                </div>
+                <div className="flex justify-between items-center w-6 h-6 rounded-full bg-white hidden">
+                  <BsMoonStars size={14} className="w-full" />
+                </div>
+              </div>
             </div>
+          </div>
+
+          <div className="flex flex-1 flex-row justify-between px-6 pb-1">
+            <div className="flex flex-1 max-w-[380px] h-full">
+              
+            </div>
+
+            <div className="flex flex-1 w-full h-full bg-red-900"></div>
+            <div className="flex flex-1 max-w-[380px] h-full bg-pink-600"></div>
+
           </div>
         </div>
       </div>
